752ad9a21e536514fbc37bdacc8454fa7e6136d4
kevin <wongkokwah@gmail.com>
2024-03-08 10:06:37 +0800

    refactor

--- app_v1.js
+++ app_v1.js
@@ -26,44 +26,93 @@ const { combine, timestamp, printf, json, prettyPrint, errors, cli } = winston.f
 // }
 
 
-console.log("levels: ", winston.config.syslog.levels)
+// console.log("levels: ", winston.config.syslog.levels)
 
-const logLevels = {
+// Define your severity levels.
+// With them, You can create log files,
+// see or hide levels based on the running ENV.
+const levels = {
   fatal: 0,
   error: 1,
   warn: 2,
   info: 3,
-  debug: 4,
-  trace: 5,
-};
+  http: 4,
+  debug: 5,
+}
 
+// This method set the current severity based on
+// the current NODE_ENV: show all the log levels
+// if the server was run in development mode; otherwise,
+// if it was run in production, show only warn and error messages.
+const level = () => {
+  const env = process.env.NODE_ENV || 'development'
+  const isDevelopment = env === 'development'
+  return isDevelopment ? 'debug' : 'warn'
+}
+
+// Define different colors for each level.
+// Colors make the log message more visible,
+// adding the ability to focus or ignore messages.
+const colors = {
+  fatal: 'cyan',
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+}
+
+// Tell winston that you want to link the colors
+// defined above to the severity levels.
+winston.addColors(colors)
+
+// Chose the aspect of your log customizing the log format.
+const format = winston.format.combine(
+  // Add the message timestamp with the preferred format
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms A' }),
+  // Tell Winston that the logs must be colored
+  winston.format.colorize({ all: true }),
+  // Define the format of the message showing the timestamp, the level and the message
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+  ),
+)
+
+// Define which transports the logger must use to print out messages.
+// In this example, we are using three different transports
+const transports = [
+  // Allow the use the console to print the messages
+  new winston.transports.Console(),
+  // Allow to print all the error level messages inside the error.log file
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  // Allow to print all the error message inside the all.log file
+  // (also the error log that are also printed inside the error.log(
+  new winston.transports.File({ 
+    filename: 'logs/all.log',
+    level: "error" 
+  }),
+]
+
+// Create the logger instance that has to be exported
+// and used to log messages.
 const logger = winston.createLogger({
-  // levels: winston.config.syslog.levels,
-  // levels: logLevels,
-  level: process.env.LOG_LEVEL || 'silly',
-  // format: winston.format.json(),
-  format: winston.format.cli(),
-  // format: combine(
-  //   // errors({stack: true}),
-  //   timestamp(),
-  //   json(),
-  //   prettyPrint()
-  //   // printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
-  // ),
-  transports: [
-    new winston.transports.Console(), 
-    new winston.transports.File({ filename: "app.log", level: "error" })
-  ],
-});
+  level: level(),
+  levels,
+  format,
+  transports,
+})
 
 
-// logger.fatal('fatal!');
+logger.fatal('fatal!');
 // logger.trace('trace!');
 logger.error('error!');
 logger.warn('warn!');
 logger.info('info!');
 logger.http('http!');
-logger.verbose('verbose!');
+// logger.verbose('verbose!');
 logger.debug('debug!');
-logger.silly('silly!');
+// logger.silly('silly!');
 
